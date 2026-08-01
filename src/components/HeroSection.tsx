7400d9a58663143b1556b5f595929d1768bb95eb
Sonuuu24 <shreyassonu2001@gmail.com>
2026-08-01 23:43:38 +0530

    New Resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -87,7 +87,7 @@ const HeroSection = () => {
             <motion.a
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
-              href="./ResumeShreyasMar05.pdf"
+              href="./Shreyas_Sonwane_Resume (1).pdf"
               download="Shreyas_Sonawane_Resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
